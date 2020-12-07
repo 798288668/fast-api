eb0a7a76a3073a8a8517d04b44aad5c74e4f36fd
fengcheng <lufengcheng@bdfint.com>
2020-12-08 00:53:17 +0800

    refactor > refactor code

--- src/main/java/com/cheng/api/modules/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- *
- * @author fengcheng
- * @version 2020/11/14
- */
-package com.cheng.api.modules;
--- src/test/java/com/cheng/test/base/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.cheng.test.base;
-
-/**
- *
- * @author fengcheng
- * @version 2020/6/29
- */
-public class BaseTest {
-}
